f8e087c1d6e6a19862657446613d973098726baf
Abdullahi <abdullahi45230@student.simad.edu.so>
2026-02-08 12:19:14 +0300

    new

--- main.js
+++ main.js
@@ -14,6 +14,18 @@ const hideNoteArea = ()=>{
     noteText.style = 'display:none';
     noteArea.classList.remove('note-now');
 }
+const addNote = (t,n) => {
+
+    notes.innerHTML +=
+    `  <div class="note">
+            <h1 class="title-text" id="title-text">${t}</h1>
+            <p class="note-blog">${n}</p>
+            <i class="fa fa-trash"></i>
+        </div>`;
+        title.value = ''; noteText.value = '';
+
+
+}
 
 noteArea.addEventListener("click", showNoteArea);
 
@@ -21,5 +33,17 @@ document.addEventListener('click',(event)=>{
     let isclicked= noteArea.contains(event.target);
     if(!isclicked){
        hideNoteArea();
+
+
+       if(title.value.length === 0 && noteText.value.length ===0){
+         return;
+       }else{
+        addNote(title.value, noteText.value);
+       }
+      
+
+
+
+       
     }
 })
